66664daea019db9047cd223f70e1866c02df7745
Paul Delta <117391498+DeltaPaul4@users.noreply.github.com>
2023-05-24 17:09:51 +0700

    nodejs

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars').engine
+const app = express();
+const port = 3000;
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', exphbs({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'rescources/views'))
+
+app.get('/home', (req, res) => {
+    res.render('home');
+});
+
+app.get('/books', (req, res) => {
+  res.render('books');
+});
+
+app.listen(port, () => {
+  console.log('Example app listening on port ${port}')
+});
